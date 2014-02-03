d7115a7f08b9e4fd1dcb447a7e630661db3582e0
Erick C <ecastillo@pernix-solutions.com>
2014-02-03 11:20:44 -0600

    Roll adds pins to turns results

--- lib/bowling/game.rb
+++ lib/bowling/game.rb
@@ -1,8 +1,14 @@
 module Bowling
   class Game
 
-    def roll(pins)
+    attr_reader :turns_results
 
+    def initialize
+      @turns_results = []
+    end
+
+    def roll(pins)
+      @turns_results << pins
     end
   end
 end
