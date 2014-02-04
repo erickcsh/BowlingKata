4f82de43eecb14c17efceeb4e895309b75601e4c
Erick C <ecastillo@pernix-solutions.com>
2014-02-04 08:56:07 -0600

    Calculates a strike

--- lib/bowling/game.rb
+++ lib/bowling/game.rb
@@ -17,20 +17,23 @@ module Bowling
       count = score = 0
       while valid_frame?(count) do
         first_try, second_try = frame_tries(count)
-        score += first_try + second_try
+        score += first_try + (second_try || 0)
+        score += @turns_results[count + 2] + @turns_results[count + 3] if first_try == 10
         score += spare_result(count) if spare?(first_try, second_try)
-        count += 2
+        count += first_try == 10 ? 1 : 2
       end
       score
     end
 
     private
     def frame_tries(count)
-      [@turns_results[count], @turns_results[count + 1]]
+      first_try = @turns_results[count]
+      second_try = first_try == 10 ? nil : @turns_results[count + 1]
+      [first_try, second_try]
     end
 
     def spare?(first_try, second_try)
-      first_try + second_try == 10
+      first_try + (second_try || 0) == 10
     end
 
     def spare_result(count)
