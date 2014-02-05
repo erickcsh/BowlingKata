2f2c791f7a04df9a494a2c7de4b01855034f6490
Erick C <ecastillo@pernix-solutions.com>
2014-02-05 09:39:03 -0600

    Tests refactor

--- spec/bowling/game_spec.rb
+++ spec/bowling/game_spec.rb
@@ -1,12 +1,14 @@
 require 'bowling/game.rb'
 
+def roll_game(plays, game)
+  plays.each { |play| play[0].times { game.roll(play[1]) } }
+end
+
 describe Bowling::Game, "#roll" do
 
   subject{ described_class.new }
 
-  after do
-    subject.roll(0)
-  end
+  after { roll_game([[1,0]], subject) }
 
   it "adds number of knocked pins to the turn result" do
     expect(subject.turns_results.size).to eq(0)
@@ -22,75 +24,49 @@ describe Bowling::Game, "#score" do
   end
 
   context "when it is an only miss game" do
-    before do
-      20.times { subject.roll(0) }
-    end
+    before { roll_game([[20,0]], subject) }
 
     it_behaves_like "a game score", 0
   end
 
   context "when it knocks 5 pins and a miss per frame" do
-    before do
-      10.times do
-        subject.roll(5)
-        subject.roll(0)
-      end
-    end
+    before { 10.times { roll_game([[1,5],[1,0]], subject) } }
 
     it_behaves_like "a game score", 50
   end
 
   context "when it knocks a spare, 5 in next turn and just misses" do
-    before do
-      3.times { subject.roll(5) }
-      17.times { subject.roll(0) }
-    end
+    before { roll_game([[3,5],[17,0]], subject) }
 
     it_behaves_like "a game score", 20
   end
 
   context "when it knocks misses, a spare in the last frame, and a 5 next" do
-    before do
-      18.times { subject.roll(0) }
-      3.times { subject.roll(5) }
-    end
+    before { roll_game([[18,0],[3,5]], subject) }
 
     it_behaves_like "a game score", 15
   end
 
   context "when knocks a strike then two 4's and just misses" do
-    before do
-      subject.roll(10)
-      2.times { subject.roll(4) }
-      16.times { subject.roll(0) }
-    end
+    before { roll_game([[1,10],[2,4],[16,0]], subject) }
 
     it_behaves_like "a game score", 26
   end
 
   context "when knocks misses, a strike in last frame and two 4's" do
-    before do
-      18.times { subject.roll(0) }
-      subject.roll(10)
-      2.times { subject.roll(4) }
-    end
+    before { roll_game([[18,0],[1,10],[2,4]], subject) }
 
     it_behaves_like "a game score", 18
   end
 
   context "when knocks misses, a strike in last frame and two 10's" do
-    before do
-      18.times { subject.roll(0) }
-      3.times { subject.roll(10) }
-    end
+    before { roll_game([[18,0],[3,10]], subject) }
 
     it_behaves_like "a game score", 30
   end
 
   context "when it is a perfect game" do
-    before do
-      12.times { subject.roll(10) }
-    end
+    before { roll_game([[12,10]], subject) }
 
     it_behaves_like "a game score", 300
   end
